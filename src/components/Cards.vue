977e23524d5022f738b245bc1fde831219811da9
Rodolfo Roman <rromanv@gmail.com>
2022-08-27 01:29:04 -0500

    added search

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,10 +1,12 @@
 <script setup>
+import Search from './Search.vue'
 import Card from './Card.vue'
 import Pagination from './Pagination.vue'
 </script>
 
 <template>
   <div class="wrapper">
+    <Search />
     <div class="cards">
       <Card v-for="n in 20" />
     </div>
@@ -14,7 +16,7 @@ import Pagination from './Pagination.vue'
 
 <style lang="postcss" scoped>
 .wrapper {
-  @apply container mx-auto py-8 flex flex-col gap-8;
+  @apply container mx-auto py-4 flex flex-col gap-4;
   .cards {
     @apply grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4;
   }
---
+++ src/components/Search.vue
@@ -0,0 +1,22 @@
+<script setup>
+import MagnifyGlass from './MagnifyGlass.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+    <input
+      type="text"
+      placeholder="Search Staff Member"
+      class="px-3 py-4 placeholder-slate-300 text-slate-600 relative rounded-xl border-0 shadow outline-none focus:outline-none focus:ring focus:ring-slate-500 w-full pl-8"
+    />
+    <span class="z-10 h-full absolute text-slate-300 text-lg right-0 pr-8 py-4">
+      <MagnifyGlass />
+    </span>
+  </div>
+</template>
+
+<style lang="postcss">
+.wrapper {
+  @apply relative flex w-full flex-wrap items-stretch;
+}
+</style>
