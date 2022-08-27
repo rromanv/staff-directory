e348f20a18f2388f7e257e6fdca1246c0b72986f
Rodolfo Roman <rromanv@gmail.com>
2022-08-27 01:15:04 -0500

    added pagination

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,15 +1,22 @@
 <script setup>
 import Card from './Card.vue'
+import Pagination from './Pagination.vue'
 </script>
 
 <template>
-  <div class="cards">
-    <Card v-for="n in 20" />
+  <div class="wrapper">
+    <div class="cards">
+      <Card v-for="n in 20" />
+    </div>
+    <Pagination />
   </div>
 </template>
 
 <style lang="postcss" scoped>
-.cards {
-  @apply grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 container mx-auto mt-8 my-4;
+.wrapper {
+  @apply container mx-auto py-8 flex flex-col gap-8;
+  .cards {
+    @apply grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4;
+  }
 }
 </style>
